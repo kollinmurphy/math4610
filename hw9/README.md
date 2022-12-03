037cc912c77c920bec495d4abfe23380741448c0
Kollin Murphy <kollin.murphy@gmail.com>
2022-12-03 09:08:34 -0700

    parts of hw9

--- hw9/README.md
+++ hw9/README.md
@@ -69,3 +69,63 @@ This is the correct result, as the smallest eigenvalue of $A$ is $64.363547$ and
 
 ## Task 2: Shifted Inverse Power Method
 
+I used the same algorithm as in Task 1, the Inverse Power Method. The only difference was that I subtracted $\mu$ from the diagonal of the matrix before running the algorithm. I used the same matrix as in Task 1, but with $\mu = 77$. The result was:
+
+$$
+A = \begin{bmatrix}
+-10 & 8 & 4 \\
+8 & 13 & 8 \\
+4 & 8 & 6
+\end{bmatrix}
+$$
+
+My test code was as follows:
+
+```
+
+int main()
+{
+  int n = 3;
+  double A[9] = {67, 8, 4, 8, 90, 8, 4, 8, 83};
+  double v[n];
+  double x0[n];
+  for (int i = 0; i < n; i++)
+  {
+    x0[i] = 1.0;
+  }
+  double mu = 77.0;
+  for (int i = 0; i < n; i++)
+    A[i * n + i] -= mu;
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      printf("%f ", A[i * n + j]);
+    }
+    printf("\n");
+  }
+  printf("\n\n");
+  double lambda = inversePowerMethod(n, A, v, x0, 1e-8, 100) + mu;
+  printf("lambda = %f\n", lambda);
+  printf("v = [");
+  for (int i = 0; i < n; i++)
+  {
+    printf("%f", v[i]);
+    if (i < n - 1)
+      printf(", ");
+  }
+  printf("]\n");
+  return 0;
+}
+```
+
+I also added $\mu$ to the resulting eigenvalue. The result was:
+
+```
+lambda = 77.837045
+v = [-0.079899, -0.617906, 1.019346]
+```
+
+This is the correct result, as the middle eigenvalue of $A$ is $77.837045$ and the corresponding eigenvector is $[-0.079899, -0.617906, 1.019346]$. This eigenvalue is different from the ones computed using the Inverse Power Method and the Power Method, which were $64.363547$ and $97.7995$, respectively.
+
+## Task 3: Finding More Eigenvalues
+
---
+++ hw9/task3.c
@@ -0,0 +1,233 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+double l2Norm(int n, double vector[n])
+{
+  double sum = 0;
+  for (int i = 0; i < n; i++)
+  {
+    sum += vector[i] * vector[i];
+  }
+  return sqrtf(sum);
+}
+
+void jacobiIteration(int n, double *A, double *b, double *x0, double *x, double tolerance, int maxIterations)
+{
+  double error = 10.0 * tolerance;
+  int iter = 0;
+  double v[n];
+  double r[n];
+  double x1[n];
+
+  for (int i = 0; i < n; i++)
+    v[i] = x0[i];
+
+  for (int i = 0; i < n; i++)
+  {
+    double sum = b[i];
+    for (int j = 0; j < n; j++)
+      sum -= A[i * n + j] * v[j];
+    r[i] = sum;
+  }
+
+  while (error > tolerance && iter < maxIterations)
+  {
+    // get next x
+    for (int i = 0; i < n; i++)
+      x1[i] = v[i] + r[i] / A[i * n + i];
+
+    // get next error
+    error = 0;
+    for (int i = 0; i < n; i++)
+    {
+      double diff = x1[i] - v[i];
+      error += diff * diff;
+    }
+    error = sqrtf(error);
+
+    iter++;
+
+    // get next residual
+    for (int i = 0; i < n; i++)
+    {
+      double sum = b[i];
+      for (int j = 0; j < n; j++)
+        sum -= A[i * n + j] * x1[j];
+      r[i] = sum;
+    }
+
+    // overwrite v
+    for (int i = 0; i < n; i++)
+      v[i] = x1[i];
+  }
+
+  for (int i = 0; i < n; i++)
+    x[i] = v[i];
+}
+
+double dotProduct(int n, double vector1[n], double vector2[n])
+{
+  double sum = 0;
+  for (int i = 0; i < n; i++)
+  {
+    sum += vector1[i] * vector2[i];
+  }
+  return sum;
+}
+
+void matrixVectorProduct(int n, int m, double *matrix, double *vector, double *result)
+{
+  double s;
+  for (int i = 0; i < n; i++)
+  {
+    result[i] = 0;
+    for (int j = 0; j < m; j++)
+    {
+      result[i] += matrix[i * n + j] * vector[j];
+    }
+  }
+}
+
+double inversePowerMethod(int n, double *A, double *result, double *x0, double tolerance, int maxIterations)
+{
+  double lambda0 = 0.0;
+  double lambda1 = 0.0;
+  double error = 10.0 * tolerance;
+  int iter = 0;
+  double y[n];
+  double z[n];
+  double w[n];
+  double initialForJacobi[n];
+  double norm;
+
+  for (int i = 0; i < n; i++)
+  {
+    initialForJacobi[i] = 1.0;
+    y[i] = 0.0;
+    w[i] = 0.0;
+  }
+
+  jacobiIteration(n, A, x0, initialForJacobi, y, tolerance, maxIterations); // y = A^-1 * x0
+
+  while (error > tolerance && iter < maxIterations)
+  {
+    norm = l2Norm(n, y);
+    for (int i = 0; i < n; i++)
+      z[i] = y[i] / norm;
+
+    jacobiIteration(n, A, z, initialForJacobi, w, tolerance, maxIterations); // w = A^-1 * z
+    lambda1 = dotProduct(n, z, w);                                           // lambda1 = z^T * w
+    error = fabs(lambda1 - lambda0);
+    lambda0 = lambda1;
+    iter++;
+    for (int i = 0; i < n; i++)
+      y[i] = w[i];
+  }
+
+  if (iter == maxIterations)
+  {
+    printf("Inverse power method did not converge in %d iterations\n", iter);
+  }
+
+  for (int i = 0; i < n; i++)
+    result[i] = y[i];
+  return 1.0 / lambda1;
+}
+
+double powerMethod(int n, double *A, double *result, double *x0, double tolerance, int maxIterations)
+{
+  double lambda0 = 0.0;
+  double lambda1 = 0.0;
+  double v0[n];
+  double v1[n];
+  double error = 10.0 * tolerance;
+  int iter = 0;
+  double norm;
+  double Az[n];
+
+  for (int i = 0; i < n; i++)
+  {
+    v0[i] = x0[i];
+    v1[i] = x0[i];
+  }
+
+  while (error > tolerance && iter < maxIterations)
+  {
+    matrixVectorProduct(n, n, A, v0, v1);
+    norm = l2Norm(n, v1);
+    for (int i = 0; i < n; i++)
+    {
+      v1[i] /= norm;
+    }
+    matrixVectorProduct(n, n, A, v1, Az);
+    lambda1 = dotProduct(n, v1, Az);
+    error = fabs(lambda1 - lambda0);
+    lambda0 = lambda1;
+    for (int i = 0; i < n; i++)
+    {
+      v0[i] = v1[i];
+    }
+    iter++;
+  }
+
+  if (iter == maxIterations)
+  {
+    printf("Power method did not converge in %d iterations\n", maxIterations);
+  }
+
+  for (int i = 0; i < n; i++)
+    result[i] = v1[i];
+
+  return lambda0;
+}
+
+int main()
+{
+  int n = 3;
+  double A[9] = {67, 8, 4, 8, 90, 8, 4, 8, 83};
+  double v[n];
+  double x0[n];
+  for (int i = 0; i < n; i++)
+  {
+    x0[i] = 1.0;
+  }
+
+  // double mu = 77.0;
+  // for (int i = 0; i < n; i++)
+  //   A[i * n + i] -= mu;
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      printf("%f ", A[i * n + j]);
+    }
+    printf("\n");
+  }
+  printf("\n\n");
+
+  double tolerance = 1e-4;
+  int maxIterations = 100;
+
+  double lambdaMax = powerMethod(n, A, v, x0, tolerance, maxIterations);
+  printf("lambdaMax = %f\n", lambdaMax);
+
+  double lambdaMin = inversePowerMethod(n, A, v, x0, tolerance, maxIterations);
+  printf("lambdaMin = %f\n", lambdaMin);
+
+  double lambdaRange = lambdaMax - lambdaMin;
+  int partitions = 10;
+  double partitionSize = lambdaRange / partitions;
+
+  for (int i = 0; i < partitions; i++)
+  {
+    double mu = lambdaMin + i * partitionSize;
+    for (int i = 0; i < n; i++)
+      A[i * n + i] -= mu;
+    double lambda = inversePowerMethod(n, A, v, x0, tolerance, maxIterations);
+    printf("lambda = %f\n", lambda + mu);
+    for (int i = 0; i < n; i++)
+      A[i * n + i] += mu;
+  }
+
+  return 0;
+}
